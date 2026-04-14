69cfe76802ba1acde6a82b54d191530597dd824d
Mickael <mhinderchied@gmail.com>
2026-04-14 14:02:41 +0200

    ajout bouton

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <main style={{ padding: "40px" }}>
-      <h1>Transport App</h1>
-
+      <h1>🚀 Mon App Transport</h1>
       <p>Bienvenue 👋</p>
+
+      <button>Cliquer ici</button>
     </main>
   );
 }
