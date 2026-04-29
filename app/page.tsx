84d0a980286af37de9c3f8aca1c90637c2443e9c
mhinderchied-design <mhinderchied@gmail.com>
2026-04-29 14:42:28 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const handleClick = () => {
-    alert("Bouton cliqué !");
-  };
-
-  return (
-    <main style={{ padding: "40px" }}>
-      <h1>🚀 Mon App Transport</h1>
-      <p>Bienvenue 👋</p>
-
-      <button onClick={handleClick}>
-        Cliquer ici
-      </button>
-    </main>
-  );
+  redirect("/protected");
 }
